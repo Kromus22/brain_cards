02ebd4db9a467de8390a9fa8581045da2d578339
Kromus22 <kromus2@gmail.com>
2023-04-20 21:43:56 +0500

    +логика переворота карточек

--- js/components/createPairs.js
+++ js/components/createPairs.js
@@ -1,4 +1,5 @@
 import { createElement } from "../helper/createElement.js"
+import { showAlert } from "./showAlert.js";
 
 export const createPairs = (app) => {
   const pairs = createElement('section', {
@@ -32,8 +33,52 @@ export const createPairs = (app) => {
   container.append(returnBtn, cardBtn);
   pairs.append(container);
 
+  //логика переворота и смены карточки
+  const cardController = data => {
+    let index = 0;
+
+    front.textContent = data[index][0];
+    back.textContent = data[index][1];
+
+    const flipCard = () => {
+      cardBtn.classList.add('card__item_flipped');
+      cardBtn.removeEventListener('click', flipCard); //убираем обработчик, чтобы исключить случайные 
+      //клики по новым карточкам. позже снова навешиваем.
+
+      setTimeout(() => {
+        cardBtn.classList.remove('card__item_flipped');
+        setTimeout(() => {
+          index++;
+
+          if (index === data.length) {
+            front.textContent = 'The end';
+            showAlert('Вернёмся к категориям!');
+
+            setTimeout(() => {
+              returnBtn.click();
+            }, 2000);
+            return;
+          }
+
+          front.textContent = data[index][0];
+          back.textContent = data[index][1];
+
+          setTimeout(() => {
+            cardBtn.addEventListener('click', flipCard);
+          }, 300); //небольшая задержка, чтобы не протыкали карточку сразу случайно.
+
+        }, 100); // во вложенном сетТаймауте установили время - 
+        //половину от анимации переворота карточки (CSS). 
+        //чтобы не было видно сменяющегося слова на карточке
+      }, 1500);
+    }
+
+    cardBtn.addEventListener('click', flipCard);
+  }
+
   const mount = data => {
     app.append(pairs);
+    cardController(data.pairs);
   }
 
   const unmount = () => {
